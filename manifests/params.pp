b63fca156e189125dc03e4dff07d9767e22dccc9
Michal Zubac <zubac@avast.com>
2016-01-07 17:12:27 +0100

    fixing forgotten chain

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,7 +2,7 @@ class iptables::params() {
   $builtins = {
     raw    => ['PREROUTING', 'OUTPUT'],
     mangle => ['PREROUTING', 'INPUT', 'FORWARD', 'OUTPUT', 'POSTROUTING'],
-    nat    => ['PREROUTING', 'OUTPUT'],
+    nat    => ['PREROUTING', 'POSTROUTING', 'OUTPUT'],
     filter => ['INPUT', 'FORWARD', 'OUTPUT'],
   }
   $targets = ['ACCEPT', 'DROP']
