33b7e8fdac4dbff3909c9d906db5dc647fc3c7ca
Michal Zubac <zubac@avast.com>
2016-07-19 19:51:55 +0200

    add rhel7 support

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,16 +1,59 @@
 class iptables::install {
   include iptables::params
 
-  package { 'iptables':
-    ensure => present,
-  }
-  ->
-  service { 'iptables':
-    ensure => running,
-    enable => true,
+  if $::osfamily == 'RedHat' {
+    if      $::operatingsystemmajrelease == 6 {
+      package { 'iptables':
+        ensure => 'present',
+      }
+      ->
+      service { 'iptables':
+        ensure => running,
+        enable => true,
+      }
+    } elsif $::operatingsystemmajrelease == 7 {
+      # do not manage firewall the os default way (firewalld)
+      if versioncmp($::puppetversion, '4.2') >= 0 {
+        service { 'firewalld':
+          ensure   => 'stopped',
+          enable   => 'mask',
+          provider => 'systemd',
+        }
+      } else {
+        service { 'firewalld':
+          ensure   => 'stopped',
+          enable   => false,
+          provider => 'systemd',
+        }
+        ->
+        # make sure firewalld is not started at any time, even through dependencies
+        exec { 'mask_firewalld_service':
+          command => '/bin/systemctl mask firewalld',
+          unless  => "/bin/bash -c '/bin/systemctl show firewalld | /bin/fgrep LoadState=masked'",
+        }
+      }
+
+      # use iptables instead
+      package { ['iptables', 'iptables-services']:
+        ensure   => 'present',
+      }
+      ->
+      Service['firewalld']
+      ->
+      # make them running
+      service { 'iptables':
+        ensure => running,
+        enable => true,
+      }
+    } else {
+      fail('Unsupported RedHat release.')
+    }
+  } else {
+    fail('Unsupported OS.')
   }
 
-  # helper script
+  # only reload script
+  # we rely on standard start and stop script from distribution
   file { "/usr/local/sbin/${iptables::params::sync_script}":
     owner  => 'root',
     group  => 'root',
